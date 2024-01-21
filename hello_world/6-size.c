8e6fb495d0c49e43ed1d698be01c5746233bbbeb
maxnchief <logandogan@gmail.com>
2024-01-21 04:48:41 +0000

    5&6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-/**
- *
+/** 
  * main - main function
  *
  * Return: ends program
@@ -10,7 +9,7 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)". sizeof(char));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
 	printf("Size of int: %lu byte(s)", sizeof(int));
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
