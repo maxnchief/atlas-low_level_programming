1187ff81704e01a3f8743945be6c8d37ee1c9303
maxnchief <logandogan@gmail.com>
2024-01-21 04:03:06 +0000

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+/**
+ *  main - main function
+ *
+ *  Return: ends the program
+ */
+
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle.);
+return (0);
+}
