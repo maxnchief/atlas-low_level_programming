867e7e3ffdb7cce1747201b49b1f29c7b526e47f
maxnchief <logandogan@gmail.com>
2024-01-22 23:06:35 +0000

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main funtion
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+
+        int i;
+
+        for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
