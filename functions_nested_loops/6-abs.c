0235184e3a3584e2df72d84629a05b0c000a0af4
maxnchief <logandogan@gmail.com>
2024-01-23 03:03:23 +0000

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _abs - function to find the absolute value of a number
+* @a: Function parameter
+* Return: a or -a
+*/
+
+int _abs(int a)
+{
+	if (a < 0);
+	else if (a > 0)
+	{
+		return (a);
+	}
+	return (-a);
+}
