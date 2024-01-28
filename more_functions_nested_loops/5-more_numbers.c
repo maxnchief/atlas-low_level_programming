2961c47941616b0020f50d3513fb3f910eb0dd7f
maxnchief <logandogan@gmail.com>
2024-01-28 22:40:15 +0000

    lets gooo

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,17 @@
 
 void more_numbers(void)
 {
+	int l;
 	int c;
 
-	c = 0;
-
-	for (c <= 0 && c >= 14;)
-	{	
-		_putchar (c * 10);
-		(c++);
-	}
+	for (l = 0 ; l < 10; l++)
+	{
+		for (c = 0 ; c <= 14 ; c++)
+		{
+			if (c > 9)
+			_putchar(c / 10 + '0');
+		_putchar(c % 10 + '0');
+		}
 	_putchar ('\n');
+	}
 }
