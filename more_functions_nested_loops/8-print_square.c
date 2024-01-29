3e5349271ab7430d4fd303e0e6bda8ea5203ccae
maxnchief <logandogan@gmail.com>
2024-01-29 01:41:17 +0000

    8 fr

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -20,8 +20,8 @@ void print_square(int size)
 		for (i = 1 ; i <= size ; i++)
 		{
 			for (j = 1 ; j <= size ; j++)
-				_putchar('#')
-			_putchar('\n')
+				_putchar('#');
+			_putchar('\n');
 		}
 	}
 }
