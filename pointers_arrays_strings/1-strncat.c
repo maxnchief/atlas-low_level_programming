807ba7c5b3095cc03920e7597106b4d868203c31
maxnchief <logandogan@gmail.com>
2024-02-05 21:48:38 +0000

    globalist

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "MAIN_H"
+/**
+ * _strncat - concanates the strings
+ *
+ * @i - parameter
+ *
+ * @j - parameter
+ *
+ * Return: 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i, j = strlen(s);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i + j] = s1[i];
+	}
+
+	s[i + j] = '\0';
+}
