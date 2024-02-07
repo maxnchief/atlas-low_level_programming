c6404bddcd97bba15156f40b23b66ba12e8ad89b
maxnchief <logandogan@gmail.com>
2024-02-07 23:50:44 +0000

    2

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverses the string
+ * @s: parameter
+ * return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
+
